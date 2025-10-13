0955ab42c884a594f1c77e6a2f324356c784f3a3
MauroMunioz <munozmauro99@gmail.com>
2025-10-13 10:23:32 -0500

    Fix styles

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   productionBrowserSourceMaps: false,
   experimental: {
-  optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
+    optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
   },
 };
 
