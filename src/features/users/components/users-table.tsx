f7c4b0fd54a5e3850e9668f82aa2089188fdcfa4
Luis Perugachi <luisprg05@gmail.com>
2025-10-14 10:12:37 -0500

    fix: reset page when user type a filter

--- src/features/users/components/users-table.tsx
+++ src/features/users/components/users-table.tsx
@@ -1,105 +1,75 @@
-"use client";
+'use client';
 
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
-import {
-  ChevronLeft,
-  ChevronRight,
-  CreditCardIcon,
-  FilterIcon,
-  MailIcon,
-  UserIcon,
-} from "lucide-react";
-import { Input } from "@/components/ui/input";
-import { useSearchParams } from "next/navigation";
-import { useDebouncedCallback } from "use-debounce";
-import { MetaPagination } from "@/features/people/domain";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
+import { ChevronLeft, ChevronRight, CreditCardIcon, FilterIcon, MailIcon, UserIcon } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import { MetaPagination } from '@/features/people/domain';
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
-  data: TData[];
-  meta?: MetaPagination & {
-    onChangePage?: (page: number) => void;
-    onNextPage?: () => void;
-    disabledNextPage: boolean;
-    onPreviousPage?: () => void;
-    disabledPreviousPage: boolean;
-    setSearchParams: (params: Record<string, any>) => void;
-  };
-  isLoading?: boolean;
-  children?: React.ReactNode;
+	columns: ColumnDef<TData, TValue>[];
+	data: TData[];
+	meta?: MetaPagination & {
+		onChangePage?: (page: number) => void;
+		onNextPage?: () => void;
+		disabledNextPage: boolean;
+		onPreviousPage?: () => void;
+		disabledPreviousPage: boolean;
+		setSearchParams: (params: Record<string, any>) => void;
+	};
+	isLoading?: boolean;
+	children?: React.ReactNode;
 }
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
+	id: string;
+	amount: number;
+	status: 'pending' | 'processing' | 'success' | 'failed';
+	email: string;
 };
 
-export function UsersTable<TData, TValue>({
-  columns,
-  data,
-  meta,
-  isLoading,
-}: DataTableProps<TData, TValue>) {
-  const searchParams = useSearchParams();
-  const table = useReactTable({
-    data: data,
-    columns: columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
+type FieldsToSearch = 'email' | 'userName' | 'fullName' | 'identification';
 
-  const start = ((meta?.currentPage ?? 0) - 1) * (meta?.itemsPerPage ?? 0) + 1;
-  const end =
-    ((meta?.currentPage ?? 0) - 1) * (meta?.itemsPerPage ?? 0) +
-    (meta?.itemCount ?? 0);
+export function UsersTable<TData, TValue>({ columns, data, meta, isLoading }: DataTableProps<TData, TValue>) {
+	const searchParams = useSearchParams();
+	const table = useReactTable({
+		data: data,
+		columns: columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
 
-  const debounceEmail = useDebouncedCallback(
-    (text: string) => meta?.setSearchParams({ email: text }),
-    500
-  );
+	let start = ((meta?.currentPage ?? 0) - 1) * (meta?.itemsPerPage ?? 0) + 1;
 
-  const debounceUserName = useDebouncedCallback(
-    (text: string) => meta?.setSearchParams({ userName: text }),
-    500
-  );
+	if (isLoading || !data?.length) start = 0;
 
-  const debounceFullName = useDebouncedCallback(
-    (text: string) => meta?.setSearchParams({ fullName: text }),
-    500
-  );
+	const end = ((meta?.currentPage ?? 0) - 1) * (meta?.itemsPerPage ?? 0) + (meta?.itemCount ?? 0);
 
-  const debounceIdentification = useDebouncedCallback(
-    (text: string) => meta?.setSearchParams({ identification: text }),
-    500
-  );
+	const debouncedSearchParams = useDebouncedCallback((field: FieldsToSearch, searchValue: string) => {
+		meta?.setSearchParams({ [field]: searchValue });
+	}, 500);
 
-  return (
-    <div className="overflow-hidden rounded-lg border p-4">
-      <div className="py-4 px-2 flex justify-between">
-        <h2>Lista de Usuarios</h2>
-        <div className="flex items-center gap-x-2">
-          <div>
-            <FilterIcon size={24} />
-          </div>
-          {/* <Select
+	const debounceFields = (field: FieldsToSearch, value: string) => {
+		// Set the 1rst page
+		meta?.onChangePage?.(1);
+		// search params
+		debouncedSearchParams(field, value);
+	};
+
+	return (
+		<div className='overflow-hidden rounded-lg border p-4'>
+			<div className='py-4 px-2 flex justify-between'>
+				<h2>Lista de Usuarios</h2>
+				<div className='flex items-center gap-x-2'>
+					<div>
+						<FilterIcon size={24} />
+					</div>
+					{/* <Select
             onValueChange={(value) => {
               meta?.setSearchParams({
                 isEmployee: value === "true" ? "true" : "false",
@@ -116,7 +86,7 @@ export function UsersTable<TData, TValue>({
               <SelectItem value={"false"}>Otros</SelectItem>
             </SelectContent>
           </Select> */}
-          {/* <Select
+					{/* <Select
             onValueChange={(value) => meta?.setSearchParams({ status: value })}
             defaultValue={"todos"}
           >
@@ -129,136 +99,111 @@ export function UsersTable<TData, TValue>({
               <SelectItem value={"inactivos"}>Inactivos</SelectItem>
             </SelectContent>
           </Select> */}
-          <div className="relative h-8 w-full flex items-center">
-            <UserIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 h-5 w-5" />
-            <Input
-              type="text"
-              placeholder="Nombre de usuario"
-              className="pl-10 pr-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2"
-              defaultValue={searchParams.get("userName") ?? ""}
-              onChange={(e) => debounceUserName(e.target.value)}
-            />
-          </div>
-          <div className="relative h-8 w-full flex items-center">
-            <MailIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 h-5 w-5" />
-            <Input
-              type="text"
-              placeholder="Buscar por email"
-              className="pl-10 pr-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2"
-              defaultValue={searchParams.get("email") ?? ""}
-              onChange={(e) => debounceEmail(e.target.value)}
-            />
-          </div>
-          <div className="relative h-8 w-full flex items-center ">
-            <CreditCardIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 h-5 w-5" />
-            <Input
-              type="text"
-              placeholder="Buscar por identificación"
-              className="pl-10 pr-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2"
-              defaultValue={searchParams.get("identification") ?? ""}
-              onChange={(e) => debounceIdentification(e.target.value)}
-            />
-          </div>
-          <div className="relative h-8 w-full flex items-center ">
-            <UserIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 h-5 w-5" />
-            <Input
-              type="text"
-              placeholder="Buscar Nombres"
-              className="pl-10 pr-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2"
-              defaultValue={searchParams.get("fullName") ?? ""}
-              onChange={(e) => debounceFullName(e.target.value)}
-            />
-          </div>
-        </div>
-      </div>
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {isLoading ? (
-            <TableRow>
-              <TableCell
-                colSpan={columns.length}
-                className="h-96 text-center animate-pulse font-semibold"
-              >
-                Loading...
-              </TableCell>
-            </TableRow>
-          ) : table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-      <div className="h-10 flex items-end justify-between mt-4">
-        <p className="text-sm text-gray-600">
-          Mostrando {start} a {end} de {meta?.totalItems} usuarios
-        </p>
-        <div className="flex items-center gap-x-2">
-          <Button
-            disabled={meta?.disabledPreviousPage}
-            onClick={() => meta?.onPreviousPage?.()}
-            variant={"outline"}
-          >
-            <ChevronLeft />
-            Anterior
-          </Button>
-          {Array.from({ length: meta?.totalPages ?? 1 }, (_, i) => (
-            <Button
-              key={i}
-              variant={"outline"}
-              className={
-                i === (meta?.currentPage ?? 1) - 1
-                  ? "bg-primary text-primary-foreground"
-                  : ""
-              }
-              onClick={() => meta?.onChangePage?.(i + 1)}
-            >
-              {i + 1}
-            </Button>
-          ))}
-          <Button
-            variant={"outline"}
-            disabled={meta?.disabledNextPage}
-            onClick={() => meta?.onNextPage?.()}
-          >
-            Siguiente
-            <ChevronRight />
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+					<div className='relative h-8 w-full flex items-center'>
+						<UserIcon className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 h-5 w-5' />
+						<Input
+							type='text'
+							placeholder='Nombre de usuario'
+							className='pl-10 pr-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2'
+							defaultValue={searchParams.get('userName') ?? ''}
+							onChange={e => debounceFields('userName', e.target.value)}
+						/>
+					</div>
+					<div className='relative h-8 w-full flex items-center'>
+						<MailIcon className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 h-5 w-5' />
+						<Input
+							type='text'
+							placeholder='Buscar por email'
+							className='pl-10 pr-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2'
+							defaultValue={searchParams.get('email') ?? ''}
+							onChange={e => debounceFields('email', e.target.value)}
+						/>
+					</div>
+					<div className='relative h-8 w-full flex items-center '>
+						<CreditCardIcon className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 h-5 w-5' />
+						<Input
+							type='text'
+							placeholder='Buscar por identificación'
+							className='pl-10 pr-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2'
+							defaultValue={searchParams.get('identification') ?? ''}
+							onChange={e => debounceFields('identification', e.target.value)}
+						/>
+					</div>
+					<div className='relative h-8 w-full flex items-center '>
+						<UserIcon className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10 h-5 w-5' />
+						<Input
+							type='text'
+							placeholder='Buscar Nombres'
+							className='pl-10 pr-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2'
+							defaultValue={searchParams.get('fullName') ?? ''}
+							onChange={e => debounceFields('fullName', e.target.value)}
+						/>
+					</div>
+				</div>
+			</div>
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map(headerGroup => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map(header => {
+								return (
+									<TableHead key={header.id}>
+										{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+									</TableHead>
+								);
+							})}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{isLoading ? (
+						<TableRow>
+							<TableCell colSpan={columns.length} className='h-96 text-center animate-pulse font-semibold'>
+								Cargando...
+							</TableCell>
+						</TableRow>
+					) : table.getRowModel().rows?.length ? (
+						table.getRowModel().rows.map(row => (
+							<TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+								{row.getVisibleCells().map(cell => (
+									<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className='h-24 text-center'>
+								No se encontró resultados.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+			<div className='h-10 flex items-end justify-between mt-4'>
+				<p className='text-sm text-gray-600'>
+					Mostrando {start > 0 && `${start} a`} {end} de {meta?.totalItems ?? 0} usuarios
+				</p>
+				<div className='flex items-center gap-x-2'>
+					<Button disabled={meta?.disabledPreviousPage} onClick={() => meta?.onPreviousPage?.()} variant={'outline'}>
+						<ChevronLeft />
+						Anterior
+					</Button>
+					{Array.from({ length: meta?.totalPages ?? 1 }, (_, i) => (
+						<Button
+							key={i}
+							variant={'outline'}
+							className={i === (meta?.currentPage ?? 1) - 1 ? 'bg-primary text-primary-foreground' : ''}
+							onClick={() => meta?.onChangePage?.(i + 1)}
+						>
+							{i + 1}
+						</Button>
+					))}
+					<Button variant={'outline'} disabled={meta?.disabledNextPage} onClick={() => meta?.onNextPage?.()}>
+						Siguiente
+						<ChevronRight />
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
