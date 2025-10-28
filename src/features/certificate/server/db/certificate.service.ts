fa8696bdb4854f8b5c1aa1673aa0268c4560004a
MauroMunioz <munozmauro99@gmail.com>
2025-10-28 10:00:18 -0500

    change api certificate for list

--- src/features/certificate/server/db/certificate.service.ts
+++ src/features/certificate/server/db/certificate.service.ts
@@ -19,8 +19,18 @@ export const updateCertificateService = async (id: string | number, request: Omi
 		.json<CreateOrUpdateHttpResponse<Certificate>>();
 };
 
-export const getCertificateByCodeService = (code: string) => {
-	return http.get('v1/1.0.0/certificate/by-code?code=' + code).json<CreateOrUpdateHttpResponse<Certificate>>();
+export const getCertificateByCodeService = async (code: string) => {
+	const response = await http.get('v1/1.0.0/certificate/certificates-by-code?code=' + code).json<CreateOrUpdateHttpResponse<Certificate[]>>();
+	
+	// La API ahora devuelve un array, tomamos el primer elemento
+	return {
+		...response,
+		data: response.data && response.data.length > 0 ? response.data[0] : undefined
+	} as CreateOrUpdateHttpResponse<Certificate>;
+};
+
+export const getCertificatesByCodeService = async (code: string) => {
+	return http.get('v1/1.0.0/certificate/certificates-by-code?code=' + code).json<CreateOrUpdateHttpResponse<Certificate[]>>();
 };
 
 export const updateCertificateShipperService = async (certificateId: string | number, request: UpdateCertificateShipperRequest) => {
