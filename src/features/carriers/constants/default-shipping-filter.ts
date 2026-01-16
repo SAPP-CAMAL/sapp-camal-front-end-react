b1c7659700b7166786eeb7adb388660a746f7130
Luis Perugachi <luisprg05@gmail.com>
2026-01-16 13:12:16 -0500

    feat: filter addressee by person name or brand

---
+++ src/features/carriers/constants/default-shipping-filter.ts
@@ -0,0 +1,9 @@
+/**
+ * Default shipping filter constants
+ * This is used to filter carriers based on predefined criteria.
+ */
+export const defaultShippingFilter = {
+	transportTypeId: 30,
+	shippingStatus: true,
+	vehicleStatus: true,
+};
