8f6c1e681d1cabf1cc5a686770a32a37b509ee89
Luis Perugachi <luisprg05@gmail.com>
2026-01-26 17:08:52 -0500

    feat: create/update vehicles

--- src/features/catalogues/server/db/catalogues.service.ts
+++ src/features/catalogues/server/db/catalogues.service.ts
@@ -1,21 +1,20 @@
-import { http } from "@/lib/ky";
+import { http } from '@/lib/ky';
 
-interface ResponseCatalogues {
-    code: number;
-    message: string;
-    data: Datum[];
+export interface ResponseCatalogues {
+	code: number;
+	message: string;
+	data: CatalogueTypes[];
 }
 
-interface Datum {
-    catalogueTypeId: number;
-    catalogueTypeCode: string;
-    catalogueId: number;
-    code: string;
-    name: string;
-    description: string;
+export interface CatalogueTypes {
+	catalogueTypeId: number;
+	catalogueTypeCode: string;
+	catalogueId: number;
+	code: string;
+	name: string;
+	description: string;
 }
 
 export function getCatalogues(code: string) {
-    return http.get("v1/1.0.0/catalogues/catalogue-type/values-by-code",
-        { searchParams: { code } }).json<ResponseCatalogues>()
+	return http.get('v1/1.0.0/catalogues/catalogue-type/values-by-code', { searchParams: { code } }).json<ResponseCatalogues>();
 }
