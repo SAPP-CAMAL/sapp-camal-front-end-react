3e84c9728140aadee9402129cc15d105d25b78be
Ariel Hurtado <arielhurtado388@gmail.com>
2026-07-24 14:19:15 -0500

    feat: pantalla de gestion de tipos de pedido por rol (order-type)

---
+++ src/features/order-type/server/db/order-type.service.ts
@@ -0,0 +1,14 @@
+import { http } from "@/lib/ky";
+import { CreateOrderTypeBody, ResponseOrderTypesService } from "@/features/order-type/domain/order-type.domain";
+
+export function createOrderTypeService(body: CreateOrderTypeBody) {
+    return http.post("v1/1.0.0/order-types", { json: body }).json()
+}
+
+export function getOrderTypesService(): Promise<ResponseOrderTypesService> {
+    return http.get("v1/1.0.0/order-types/all").json()
+}
+
+export function deleteOrderTypeService(id: number) {
+    return http.delete(`v1/1.0.0/order-types/${id}`).json()
+}
